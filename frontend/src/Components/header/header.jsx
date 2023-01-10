3cef0838538498450ae4631eb7efd4bd8ec3aace
Nana Kofi Okae <okaen@vcu.edu>
2023-01-10 00:26:12 -0500

    finished navigation bar

---
+++ frontend/src/Components/header/header.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./header.css";
+
+const Header = () => {
+  const [Toggle, setToggle] = useState(false);
+
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">
+          Nana Kofi Okae
+        </a>
+
+        <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="#home" className="nav__link">
+                <i className="uil uil-estate nav__icon"></i>Home
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="uil uil-user nav__icon"></i>About
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                <i className="uil uil-file-alt nav__icon"></i> Skills
+              </a>
+            </li>
+            {/* <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                <i className="uil uil nav__icon"></i> 
+              </a>
+            </li> */}
+
+            <li className="nav__item">
+              <a href="#work" className="nav__link">
+                <i className="uil uil-dialpad nav__icon"></i>Work
+              </a>
+            </li>
+          </ul>
+
+          <i
+            class="uil uil-times nav__close"
+            onClick={() => setToggle(!Toggle)}></i>
+        </div>
+        <div className="nav__toggle" onClick={() => setToggle(!Toggle)}>
+          <i class="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
